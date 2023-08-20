d82af8546b71868e9edc6f9ba69ab0b6cd08f5bb
mxgc <mx@great.cat>
2023-08-20 14:13:31 -0700

    cp6 - building a component the redwood way

---
+++ web/src/components/Comment/Comment.test.jsx
@@ -0,0 +1,26 @@
+import { render, screen } from '@redwoodjs/testing/web'
+
+import Comment from './Comment'
+
+//   Improve this test with help from the Redwood Testing Doc:
+//    https://redwoodjs.com/docs/testing#testing-components
+
+describe('Comment', () => {
+  it('renders successfully', () => {
+    const comment = {
+      name: 'Rob Cameron',
+      body: 'This is the first comment!',
+      createdAt: '2020-01-01T12:34:56Z',
+    }
+
+    render(<Comment comment={comment} />)
+
+    expect(screen.getByText(comment.name)).toBeInTheDocument()
+    expect(screen.getByText(comment.body)).toBeInTheDocument()
+
+    const dateExpected = screen.getByText('1 January 2020')
+    expect(dateExpected).toBeInTheDocument()
+    expect(dateExpected.nodeName).toEqual('TIME')
+    expect(dateExpected).toHaveAttribute('datetime', comment.createdAt)
+  })
+})
