42870d3830f4104561b63b9c7d4c3cf4895a455e
mxgc <mx@great.cat>
2023-09-13 14:56:37 -0700

    cp5 complete - our first test

--- web/src/components/ArticlesCell/ArticlesCell.js
+++ web/src/components/ArticlesCell/ArticlesCell.js
@@ -18,6 +18,8 @@ export const Empty = () => <div>Empty</div>
 export const Failure = ({ error }) => <div>Error: {error.message}</div>
 
 export const Success = ({ articles }) => {
+  console.log(articles)
+
   return (
     <div className="space-y-10">
       {articles.map((article) => (
