657bd9a896933a72c9baabbb6a7cec28fb7828d8
mxgc <mx@great.cat>
2023-09-13 15:58:35 -0700

    cp6 - multiple comments

--- web/src/components/Article/Article.test.js
+++ web/src/components/Article/Article.test.js
@@ -1,4 +1,6 @@
-import { render, screen } from '@redwoodjs/testing'
+import { render, screen, waitFor } from '@redwoodjs/testing'
+
+import { standard } from 'src/components/CommentsCell/CommentsCell.mock'
 
 import Article from './Article'
 
@@ -26,5 +28,29 @@ describe('Article', () => {
         'Neutra tacos hot chicken prism raw denim, put a bird on it enamel pin post-ironic vape cred DIY. Str...'
       )
     ).toBeInTheDocument()
+  })
+
+  it('renders comments when displaying a full blog post', async () => {
+    const comment = standard().comments[0]
+
+    // the cell associated with Article compoment is mocked rather using actual graphql
+    render(<Article article={ARTICLE} summary={false} />)
+
+    // `waitFor` wait for things like gql queries to finish running before checking for what's rendered
+    // since `Article` renders `CommentsCell` we need to wait for `Success` component of `CommentsCell` to be rendered
+    await waitFor(() => {
+      expect(screen.getByText(comment.body)).toBeInTheDocument()
+    })
+  })
+
+  it('does not render comments when displaying a summary blog post', async () => {
+    const comment = standard().comments[0]
+
+    // the cell associated with Article compoment is mocked rather using actual graphql
+    render(<Article article={ARTICLE} summary={true} />)
+
+    await waitFor(() => {
+      expect(screen.queryByText(comment.body)).not.toBeInTheDocument()
+    })
   })
 })
